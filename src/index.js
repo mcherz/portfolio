e7506c9d7ea0e2f64fdc069fbef00aeadfcd1eb8
Matt Herz <mcherz@gmail.com>
2017-10-03 22:41:46 -0700

    webpack app setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"; //eslint-disable-line
+import ReactDOM from "react-dom";
+import Welcome from "components/welcome";
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Welcome/>, app);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+let path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve("./src"),
+      path.resolve("./node_modules")
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist")
+  }
+};
