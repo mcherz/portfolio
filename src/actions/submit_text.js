b1a4b87715e07179732526b728e83498fdeb8535
Matt Herz <mcherz@gmail.com>
2018-01-07 20:54:43 -0800

    wait, seriously, voice is working.

--- src/actions/submit_text.js
+++ src/actions/submit_text.js
@@ -9,7 +9,10 @@ const submitText = (text) => (dispatch, getState) => {
     dispatch(setButtonActive(false))
     dispatch(addResponse(constants.USER_QUERY, text))
     setTimeout(() => {
-      dispatch(addResponse(constants.APP_RESPONSE, getAppResponse(text)))
+      const response = getAppResponse(text)
+      dispatch(addResponse(constants.APP_RESPONSE, response.jsx))
+      let utterThis = new SpeechSynthesisUtterance(response.plainText)
+      speechSynthesis.speak(utterThis)
       dispatch(setButtonActive(true))
     }, constants.CONVERSATION_LAG)
   }
--- src/helpers/get_app_response.js
+++ src/helpers/get_app_response.js
@@ -1,8 +1,40 @@
+import React from "react" //eslint-disable-line
 import ElizaBot from "elizabot"
 
+import magic from "helpers/magic_responses"
+
 const getAppResponse = (inputString) => {
-  const eliza = new ElizaBot()
-  return eliza.transform(inputString)
+  const lowString = inputString.toLowerCase()
+  switch (true) {
+    case lowString.match("resume") !== null:
+      return magic.resume
+    case lowString.match("linkedin") !== null:
+      return magic.linkedin
+    case lowString.match("twitter") !== null:
+      return magic.twitter
+    case lowString.match("alexa") !== null:
+      return magic.alexa
+    case lowString.match("cortana") !== null:
+      return magic.cortana
+    case lowString.match("google") !== null:
+      return magic.google
+    case lowString.match("siri") !== null:
+      return magic.siri
+    case lowString.match("eliza") !== null:
+      return magic.eliza
+    case lowString.match("open source") !== null:
+    case lowString.match("source code") !== null:
+      return magic.open_source
+    case lowString.match("email") !== null:
+      return magic.email
+    default:
+      const eliza = new ElizaBot()
+      const returnString = eliza.transform(inputString)
+      return {
+        jsx: <span>{returnString}</span>,
+        plainText: returnString
+      }
+  }
 }
 
 export default getAppResponse
