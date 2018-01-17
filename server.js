4d7ba6ae3c67431296f08195de5f9c77258b8fba
Matt Herz <mcherz@gmail.com>
2018-01-16 21:10:03 -0800

    file structure cleanup

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express")
-let app = express()
+
+let server = express()
 
 const port = process.env.PORT || 8080
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + "/dist"))
+server.use(express.static(__dirname + "/dist"))
 
 // set the home page route
-app.get("/", function(req, res) {
+server.get("/", function(req, res) {
   res.send("index.html")
 })
 
-app.listen(port, function() {
-  console.log("Our app is running on " + port)
+server.listen(port, function() {
+  console.log("server is running on " + port)
 })
